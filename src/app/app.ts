bdff6712707a94fcf92b06c42793b96ec45808b8
Nam Pham <allstarsmen@gmail.com>
2026-05-11 08:11:29 +0700

    Remove unused module

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { TodoInput } from './todo-input/todo-input';
 import { TodoList } from './todo-list/todo-list';
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet,
     TodoInput,
     TodoList
   ],
